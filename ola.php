4b4bb3ccc984f4634f4aafda3bd70a2295547f7c
dodimaciel <dodymaciel@gmail.com>
2016-12-10 00:49:04 -0200

    Initial commit

---
+++ ola.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+    <h1>Testar.</h1>
+    <?php
+        echo Ola mundo;
+    ?>
+</body>
+</html>
